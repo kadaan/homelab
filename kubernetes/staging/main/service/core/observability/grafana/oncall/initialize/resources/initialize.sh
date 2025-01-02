a173d53f1e7629e8879dc43f2813dcd7ae13f770
Joel Baranick <jbaranick@gmail.com>
2025-01-01 20:31:08 -0800

    Add logging

--- kubernetes/staging/main/service/core/observability/grafana/oncall/initialize/resources/initialize.sh
+++ kubernetes/staging/main/service/core/observability/grafana/oncall/initialize/resources/initialize.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-curl -v -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/settings" -H "Content-Type: application/json" -d "{\"enabled\":true, \"jsonData\":{\"stackId\":5, \"orgId\":100, \"onCallApiUrl\":\"http://${GRAFANA_ONCALL_HOST:?}:${GRAFANA_ONCALL_PORT:?}/\", \"grafanaUrl\":\"https://${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/\"}}"
-curl -v -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/resources/plugin/install"
+echo "Calling: curl -v \"https://${GRAFANA_ADMIN_USERNAME:?}:***********${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/settings\" -H \"Content-Type: application/json\" -d \"{\\\"enabled\\\":true, \\\"jsonData\\\":{\\\"stackId\\\":5, \\\"orgId\\\":100, \\\"onCallApiUrl\\\":\\\"http://${GRAFANA_ONCALL_HOST:?}:${GRAFANA_ONCALL_PORT:?}/\\\", \\\"grafanaUrl\\\":\\\"https://${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/\\\"}}\""
+curl -v "https://${GRAFANA_ADMIN_USERNAME:?}:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/settings" -H "Content-Type: application/json" -d "{\"enabled\":true, \"jsonData\":{\"stackId\":5, \"orgId\":100, \"onCallApiUrl\":\"http://${GRAFANA_ONCALL_HOST:?}:${GRAFANA_ONCALL_PORT:?}/\", \"grafanaUrl\":\"https://${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/\"}}"
+
+echo "Calling: curl -v -X POST \"https://${GRAFANA_ADMIN_USERNAME:?}::?}:***********@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/resources/plugin/install\""
+curl -v -X POST "https://${GRAFANA_ADMIN_USERNAME:?}:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/resources/plugin/install"
