bfa9884f4cb4968da236748040c6808814e36db4
Joel Baranick <jbaranick@gmail.com>
2025-01-01 19:41:11 -0800

    Increase curl logging

--- kubernetes/staging/main/service/core/observability/grafana/oncall/initialize/resources/initialize.sh
+++ kubernetes/staging/main/service/core/observability/grafana/oncall/initialize/resources/initialize.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-curl -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/settings" -H "Content-Type: application/json" -d "{\"enabled\":true, \"jsonData\":{\"stackId\":5, \"orgId\":100, \"onCallApiUrl\":\"http://${GRAFANA_ONCALL_HOST:?}:${GRAFANA_ONCALL_PORT:?}/\", \"grafanaUrl\":\"https://${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/\"}}"
-curl -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/resources/plugin/install"
+curl -v -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/settings" -H "Content-Type: application/json" -d "{\"enabled\":true, \"jsonData\":{\"stackId\":5, \"orgId\":100, \"onCallApiUrl\":\"http://${GRAFANA_ONCALL_HOST:?}:${GRAFANA_ONCALL_PORT:?}/\", \"grafanaUrl\":\"https://${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/\"}}"
+curl -v -X POST "https://admin:${GRAFANA_ADMIN_PASSWORD:?}@${GRAFANA_HOST:?}:${GRAFANA_PORT:?}/api/plugins/grafana-oncall-app/resources/plugin/install"
